269bcb6d960f2c3e860cdf21ee91547876bc2081
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-03-30 17:00:35 +0530

    (ft): Router

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,32 @@ import Footer from "./sections/Footer";
 import Navbar from "./sections/Navbar";
 import Wrapper from "./sections/Wrapper";
 import "./scss/index.scss";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Search from "./pages/Search";
+import MyList from "./pages/MyList";
+import About from "./pages/About";
+import Compare from "./pages/Compare";
+import Pokemon from "./pages/Pokemon";
 
 const App = () => {
   return (
     <div className="main-container">
       <Background />
-      <div className="app">
-        <Navbar />
-        <Wrapper />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route element={<Search />} path="/search" />
+            <Route element={<MyList />} path="/list" />
+            <Route element={<About />} path="/about" />
+            <Route element={<Compare />} path="/compare" />
+            <Route element={<Pokemon />} path="/pokemon/:id" />
+            <Route element={<Navigate to="/pokemon/1" />} path="*" />
+          </Routes>
+          <Wrapper />
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,13 +1,41 @@
 import pokeballIcon from "../assets/pokeball-icon.png";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const NavigationRoutes = [
+    {
+      name: "Search",
+      route: "/search",
+    },
+    {
+      name: "Compare",
+      route: "/compare",
+    },
+    {
+      name: "Pokemon",
+      route: "/pokemon",
+    },
+    {
+      name: "About",
+      route: "/about",
+    },
+  ];
+
   return (
     <nav>
       <div className="block">
         <img src={pokeballIcon} alt="pokeball-icon" />
       </div>
-      <div className="data"></div>
+      <div className="data">
+        {NavigationRoutes.map(({ name, route }, index) => {
+          return (
+            <Link to={route} key={index}>
+              <li>{name}</li>
+            </Link>
+          );
+        })}
+      </div>
       <div className="block">
         <GiHamburgerMenu />
       </div>
