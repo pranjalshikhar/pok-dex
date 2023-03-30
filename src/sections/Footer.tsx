b20316ac1f09aa37dc1c980d4309fd3c63605a36
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-03-30 16:42:16 +0530

    (ft): Footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,5 +1,15 @@
+import { MdOutlinePowerSettingsNew } from "react-icons/md";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="block"></div>
+      <div className="data"></div>
+      <div className="block">
+        <MdOutlinePowerSettingsNew />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
