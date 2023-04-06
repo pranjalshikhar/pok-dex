1a440da4f971674700cc150b1232c91994fc02a4
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-04-06 23:46:19 +0530

    (ft): Sign Out Functionality

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,12 +1,22 @@
+import { signOut } from "firebase/auth";
 import { MdOutlinePowerSettingsNew } from "react-icons/md";
+import { firebaseAuth } from "../utils/firebaseConfig";
+import { useAppDispatch } from "../app/hooks";
+import { setToasts, setUserStatus } from "../app/slices/AppSlice";
 
 const Footer = () => {
+  const dispatch = useAppDispatch();
+  const logOutUser = () => {
+    signOut(firebaseAuth);
+    dispatch(setUserStatus(undefined));
+    dispatch(setToasts("Logged out successfully."));
+  };
   return (
     <footer>
       <div className="block"></div>
       <div className="data"></div>
       <div className="block">
-        <MdOutlinePowerSettingsNew />
+        <MdOutlinePowerSettingsNew onClick={logOutUser} />
       </div>
     </footer>
   );
