600cf577929b76042b61fb4bd1166a8260fbf632
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-04-07 00:06:40 +0530

    (ft): About Page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,23 @@
 import Wrapper from "../sections/Wrapper";
+import avatarImage from "../assets/self.jpg";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="profile">
+      <img src={avatarImage} alt="Pranjal Shikhar" className="profile-image" />
+      <h1 className="profile-text">Hye, I am Pranjal Shikhar ⚡</h1>
+      <h2 className="profile-text">The creator of this awesome pokédex.</h2>
+      <div className="profile-links">
+        <a href="https://www.github.com/pranjalshikhar">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/pranjalshikhar">
+          <FaLinkedin />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Wrapper(About);
