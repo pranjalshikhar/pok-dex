6ef90976606a0f01c7b99cccb07f83ea3d0be14e
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2024-03-02 22:59:34 +0530

    (style): mobile responsiveness

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,27 @@
 import Wrapper from "../sections/Wrapper";
-import avatarImage from "../assets/self.jpg";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaGlobe, FaLinkedin } from "react-icons/fa";
 
 const About = () => {
   return (
     <div className="profile">
-      <img src={avatarImage} alt="Pranjal Shikhar" className="profile-image" />
-      <h1 className="profile-text">Hye, I am Pranjal Shikhar ⚡</h1>
-      <h2 className="profile-text">The creator of this awesome pokédex.</h2>
+      <img
+        src={
+          "https://pranjalshikhar.vercel.app/_next/static/media/Avatar-black.dd6a4407.svg"
+        }
+        alt="Pranjal Shikhar"
+        className="profile-image"
+      />
+      <h1 className="profile-text">hey there, myself pranjal shikhar 〽️</h1>
+      <h2 className="profile-text">creator of this pokédex app.</h2>
       <div className="profile-links">
         <a href="https://www.github.com/pranjalshikhar">
           <FaGithub />
         </a>
         <a href="https://www.linkedin.com/in/pranjalshikhar">
           <FaLinkedin />
+        </a>
+        <a href="https://pranjalshikhar.vercel.app">
+          <FaGlobe />
         </a>
       </div>
     </div>
